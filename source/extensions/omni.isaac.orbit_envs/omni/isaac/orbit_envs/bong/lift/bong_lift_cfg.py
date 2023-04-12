938143321588558ecfd7fb1bb6bf1736c03ac13e
BongGyeong Park <39259734+PARKBONG@users.noreply.github.com>
2023-04-13 02:43:40 +0900

    No height penalty

--- source/extensions/omni.isaac.orbit_envs/omni/isaac/orbit_envs/bong/lift/bong_lift_cfg.py
+++ source/extensions/omni.isaac.orbit_envs/omni/isaac/orbit_envs/bong/lift/bong_lift_cfg.py
@@ -161,7 +161,7 @@ class RewardsCfg:
 
     # -- robot-centric
     reaching_object_position_l2 = {"weight": 50}
-    reaching_object_height = {"weight": 50}
+    # reaching_object_height = {"weight": 50}
     # reaching_object_position_exp = {"weight": 2.5, "sigma": 0.25}
     # reaching_object_position_tanh = {"weight": 2.5, "sigma": 0.1}
     # penalizing_arm_dof_velocity_l2 = {"weight": 1e-5}
@@ -177,7 +177,7 @@ class RewardsCfg:
     # lifting_object_success = {"weight": 3.5, "threshold": 0.08}
     # lifting_object_desired_success = {"weight" : 2}
     bong_catch_object = {"weight": 200}
-    bong_catch_failure = {"weight": 25}
+    bong_catch_failure = {"weight": 50}
 
 
 @configclass
