985a030f82bd2db5c6511a86701f77756c7b2386
BongGyeong <39259734+PARKBONG@users.noreply.github.com>
2023-04-18 14:37:43 +0900

    cfg .

--- source/extensions/omni.isaac.orbit_envs/omni/isaac/orbit_envs/bong/lift/bong_lift_cfg.py
+++ source/extensions/omni.isaac.orbit_envs/omni/isaac/orbit_envs/bong/lift/bong_lift_cfg.py
@@ -143,7 +143,7 @@ class ObservationsCfg:
         # -- object state
         # object_positions = {"scale": 1.0}
         # object_orientations = {"scale": 1.0}
-        object_relative_tool_positions = {"scale": 10.0}
+        object_relative_tool_positions = {"scale": 1.0}
         # object_relative_tool_orientations = {"scale": 1.0}
         # -- object desired state
         # object_desired_positions = {"scale": 1.0}
@@ -151,7 +151,7 @@ class ObservationsCfg:
         # arm_actions = {"scale": 1.0}
         tool_actions = {"scale": 10.0}
         # bong_obj_to_desire = {"scale": 1.0}
-        bong_obj_height = {"scale": 100.0}
+        bong_obj_height = {"scale": 1.0}
 
     # global observation settings
     return_dict_obs_in_group = False
